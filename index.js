2008b6d1b0fd569056b7c3e0a6513299c403c1d9
Farihaakter <yellowfariha@gmail.com>
2021-06-26 16:48:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = 4000;
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ssth5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const blogCollection = client.db("retro-blogs").collection("blogs");
+    const adminCollection = client.db("retro-blogs").collection("admin");
+
+    app.post('/addBlog', (req, res) => {
+        const newBlog = req.body;
+        blogCollection.insertOne(newBlog)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.get('/admin', (req, res) => {
+        adminCollection.find({ email: req.query.email })
+            .toArray((err, admin) => {
+                if (admin.length === 0) {
+                    res.send([])
+                }
+                else {
+                    adminCollection.find()
+                        .toArray((err, documents) => {
+                            res.send(documents)
+                        })
+                }
+            })
+    })
+
+    app.get('/manageBlog', (req, res) => {
+        blogCollection.find()
+            .toArray((err, blogs) => {
+                res.send(blogs)
+            })
+    })
+
+    app.delete('/delete/:id', (req, res) => {
+        const id = ObjectId(req.params.id);
+        console.log("delete this", id)
+        blogCollection.findOneAndDelete({ _id: id })
+            .then(documents => res.send(!!documents.value))
+
+    })
+
+    app.get('/allBlogs', (req, res) => {
+        blogCollection.find()
+            .toArray((err, blogs) => {
+                res.send(blogs)
+            })
+    })
+
+    app.get('/singlePost/:id', (req, res) => {
+        blogCollection.find({ _id: ObjectId(req.params.id) })
+            .toArray((err, blog) => {
+                res.send(blog)
+            })
+    })
+});
+
+
+app.get('/', function (req, res) {
+    res.send('hello world....')
+})
+
+app.listen(process.env.PORT || port)
