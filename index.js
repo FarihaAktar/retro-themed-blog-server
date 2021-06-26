20144e34e58566dabf3e08b4ff3ba87da6ad12a3
Farihaakter <yellowfariha@gmail.com>
2021-06-26 17:15:47 +0600

    added changes

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const port = 4000;
 
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ssth5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
